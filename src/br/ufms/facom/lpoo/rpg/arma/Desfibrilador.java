bdd0ac8a2eced74700272024a6bead0a266f48ba
Yan Uehara <yanuehara@hotmail.com>
2016-09-03 18:40:52 -0400

    Added Desfibrilador implementation

---
+++ src/br/ufms/facom/lpoo/rpg/arma/Desfibrilador.java
@@ -0,0 +1,16 @@
+package br.ufms.facom.lpoo.rpg.arma;
+
+public abstract class Desfibrilador implements Arma {
+
+	@Override
+	public int getAlcance() {
+		return 0;
+	}
+
+	@Override
+	public int getDano() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
