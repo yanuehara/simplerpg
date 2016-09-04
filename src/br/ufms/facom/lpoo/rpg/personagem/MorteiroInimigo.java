db01d43ffb9b161fe6e5f5271ae7e8f4c0389343
flavioshinzato <f.a.m.shinzato@gmail.com>
2016-09-03 20:45:06 -0400

    Correções sobre a classe Morteiro
    Exclusão da classe MorteiroAliado(não existe) e alteração de
    MorteiroPersonagem para MorteiroInimigo.

--- src/br/ufms/facom/lpoo/rpg/personagem/MorteiroInimigo.java
+++ src/br/ufms/facom/lpoo/rpg/personagem/MorteiroInimigo.java
@@ -1,19 +1,129 @@
 package br.ufms.facom.lpoo.rpg.personagem;
 
-import br.ufms.facom.lpoo.rpg.personagem.MorteiroPersonagem;
+import br.ufms.facom.lpoo.rpg.arma.Arma;
 import br.ufms.facom.lpoo.rpg.arma.Morteiro;
 
-public class MorteiroInimigo extends MorteiroPersonagem {
+/**
+ * Personagem morteiro.
+ * <p>
+ * Esta classe possui todos os atributos e métodos básicos do personagem morteiro.
+ *
+ */
+public class MorteiroInimigo implements Personagem{
 	/**
-	 * Constrói um MorteiroAliado que possui uma morteiro como arma.
+	 * Arma do morteiro.
+	 */
+	protected Arma arma;
+
+	/**
+	 * Nível de vida do morteiro.
+	 */
+	private int vida;
+
+	/**
+	 * Nome do morteiro.
+	 */
+	private String nome;
+
+	/**
+	 * Posição do morteiro no tabuleiro.
+	 */
+	private Posicao posicao;
+	
+	/**
+	 * Probabilidade do morteiro atacar.
+	 */
+	private int ataque;
+	
+	/**
+	 * Probabilidade do morteiro de defender.
+	 */
+	private int defesa;
+	
+	/**
+	 * Distância que o morteiro pode se mover no campo.
+	 */
+	private int velocidade;
+	
+	/**
+	 * Cria um morteiro com o nome dado.
+	 * 
 	 * @param nome
-	 * @param morteiro
+	 */
+	public MorteiroInimigo(String nome) {
+		this.nome = nome;
+		vida = 5;
+		ataque = 4;
+		defesa = 2;
+		posicao = new Posicao();
+		velocidade = 0;
+	}
+
+	/**
+	 * Cria um morteiro com o nome e posição dados.
+	 * 
+	 * @param nome
 	 * @param x
 	 * @param y
 	 */
 	public MorteiroInimigo(String nome, Morteiro morteiro, int x, int y) {
-		super(nome, x, y);
+		this.nome = nome;
+		posicao = new Posicao(x, y);
 		this.arma = morteiro;
 	}
 
+	@Override
+	public int getDefesa() {
+		return this.defesa;
+	}
+
+	@Override
+	public int getAtaque() {
+		return this.ataque;
+	}
+
+	@Override
+	public int getVelocidade() {
+		return this.velocidade;
+	}
+
+	@Override
+	public int getVida() {
+		return vida;
+	}
+
+	@Override
+	public Arma getArma() {
+		return arma;
+	}
+
+	@Override
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public int getX() {
+		return posicao.x;
+	}
+
+	@Override
+	public int getY() {
+		return posicao.y;
+	}
+
+	@Override
+	public void setX(int x) {
+		posicao.x = x;
+	}
+
+	@Override
+	public void setY(int y) {
+		posicao.y = y;
+	}
+
+	@Override
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
 }
